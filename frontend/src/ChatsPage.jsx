830bcdbce5f9d9a8d566cd5176b60c469d0f599f
NikitaTorbenko <2015nekitciti@gmail.com>
2023-05-17 15:34:59 +0300

    вроде complete

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,20 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "8ce77fc0-c860-4bab-939c-4840f579fb9a",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+export default ChatsPage;
